49153783600b7539f5da0003e465dd72aa0df38a
Giselle_Design <gh.saadat63@gmail.com>
2020-07-21 19:56:49 -0700

    add routes & actions for Questions resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,35 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  get("/hello_world", to: "welcome#hello_world")
 
- get "/", to: "welcome#root"
+ # get "/", to: "welcome#root", as: "root"
+
+ root "welcome#root"
 
  get "/contact_us", to: "welcome#contact_us"
 
  post "/process_contact", to: "welcome#process_contact"
+
+
+
+ get "/questions/new", {to: "questions#new", as: :new_questions}
+
+ post "/questions", {to: "questions#create", as: :questions}
+
+ get "/questions/:id", {to: "questions#show", as: :question}
+
+ get "/questions", {to: "questions#index"}
+
+ get "/questions/:id/edit", {to: "questions#edit", as: :edit_question}
+
+ patch "/questions/:id", {to: "questions#update"}
+
+ delete "/questions/:id", {to: "questions#destroy"}
+
+ # resources :questions
+
 end
+
+
+# get 'profile', to: 'users#show'
+#
+# get 'profile', action: :show, controller: 'users'
